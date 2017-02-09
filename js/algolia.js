bb94b25887be76310297a4c217ddb0da6eb20fd9
marc wong <marcw@uci.edu>
2017-02-09 00:15:22 -0800

    require?

---
+++ js/algolia.js
@@ -0,0 +1,9 @@
+var client = algoliasearch('A2CZQP61F0', '78429c5ee9cb638cdfc5bac29920f907');
+var index = client.initIndex('quotes');
+var quotesJSON = require('./data/board-data.json');
+
+index.addObjects(quotesJSON, function(err, content) {
+  if(err){
+    console.error(err);
+  }
+});
